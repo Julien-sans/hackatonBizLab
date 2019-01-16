f277e93affd4ed17b2ae8f1364c1ed87f267e13d
Julien SANS <julien_ss@hotmail.fr>
2019-01-16 18:54:57 +0100

    implementation redux sur formulaire

---
+++ biz-lab-project/src/reducers/forms.js
@@ -0,0 +1,29 @@
+import project from '../data/project.json'
+import { EDITOR_INPUT_FORM } from '../actions/forms';
+import { CREATE_FORM } from '../actions/forms';
+
+const initialState = {
+  name: '',
+  description: '',
+  type: '',
+  competences: '',
+  lien: '',
+  equipe: '',
+  initiateur: '',
+  project: project
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case EDITOR_INPUT_FORM:
+      const { key, value } = action
+      return { ...state, [key]: value }
+    case CREATE_FORM:
+      const form = action.form
+      return { ...state, project: [...state.project, form]}
+    default:
+    return state
+  }
+}
+
+export default reducer;
