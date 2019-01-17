d5e11111c95337d20cab5a68aae6dc2754a9611a
Julien SANS <julien_ss@hotmail.fr>
2019-01-17 11:51:19 +0100

    creation local storage pour stocker les projects

--- biz-lab-project/src/index.js
+++ biz-lab-project/src/index.js
@@ -7,11 +7,20 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import { loadState, saveState } from './components/connectivity/localStorage'
+
+const persistedState = loadState();
 
 const store = createStore(
   reducer,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+store.subscribe(() => {
+  saveState(store.getState());
+  console.log(store.getState())
+});
 
 ReactDOM.render(
   <Provider store={store}>
@@ -19,7 +28,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: http://bit.ly/CRA-PWA
+  serviceWorker.unregister();
